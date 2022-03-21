72c4d2dd88322a05f55246fdab5d66f98858d2fb
mihaGP800 <mihagoroh@gmail.com>
2022-03-21 12:28:50 +0300

    15-Select_onKeuUpHandler_useEffect_setHover

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, KeyboardEvent, useEffect} from 'react';
 import s from './Select.module.css'
 
 export type ItemType = {
@@ -14,21 +14,54 @@ type SelectType = {
 }
 
 export const Select: React.FC<SelectType> = ({items, select, setSelect, ...props}) => {
-    let [collapsed, setCollapsed] = useState(false)
+    let [collapsed, setCollapsed] = useState<boolean>(false)
+    let [titleHover, setTitleHover] = useState(select)
 
-    const onCollapsed = () => setCollapsed(!collapsed);
+    const toggleCollapsed = () => setCollapsed(!collapsed);
+    const offCollapsed = () => setCollapsed(false);
+
+    const onclickItem = (title: string) => {
+        setSelect(title)
+        setCollapsed(false)
+    }
+
+    const onHoverItem = (title: string) => {
+        setTitleHover(title)
+    }
+
+    const onKeyUpHandler = (e: KeyboardEvent<HTMLDivElement>) => {
+        if (e.key === 'ArrowDown' || e.key === 'ArrowUp') {
+            for (let i = 0; i < items.length; i++) {
+                if (items[i].title === titleHover) {
+                    e.key === 'ArrowDown' && items[i + 1] && setSelect(items[i + 1].title)
+                    e.key === 'ArrowUp' && items[i - 1] && setSelect(items[i - 1].title)
+                    return
+                }
+            }
+            setSelect(items[0].title)
+        }
+        if (e.key === 'Enter' || e.key === 'Escape') {
+            offCollapsed()
+        }
+    }
+    useEffect(() => {
+        setTitleHover(select)
+    }, [select])
 
     return (
-        <div onBlur={onCollapsed} tabIndex={1} className={s.divHeader}>
-            <div className={`${s.shadow} ${s.title}`} onClick={onCollapsed}>{select}▼
+        <div className={s.divHeader}
+            // onBlur={offCollapsed} tabIndex={1}
+        >
+            <div className={`${s.shadow} ${s.title}`} onClick={toggleCollapsed} onKeyUp={onKeyUpHandler}
+                 tabIndex={1}>{select}▼
             </div>
             {collapsed &&
                 <div>
                     {items.map((el, index) =>
                         <div
-                            // onBlur={onCollapsed} tabIndex={index}
-                            className={`${s.select} ${s.title}`}
-                            onClick={() => setSelect(el.title)}
+                            onMouseEnter={() => onHoverItem(el.title)}
+                            className={`${s.select} ${s.title}` + ' ' + (el.title === titleHover ? s.titleHover : '')}
+                            onClick={() => onclickItem(el.title)}
                             key={el.value}>➣ {el.title}
                         </div>)}
                 </div>}
